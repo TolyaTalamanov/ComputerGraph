9b6836d478e72be8a6346b98cff6185642822feb
TolyaTalamanov <talamtolik@gmail.com>
2017-03-14 23:15:07 +0300

    try fix Median

--- TalamanovBatanina_2/TalamanovBatanina_2/MedianFilter.cs
+++ TalamanovBatanina_2/TalamanovBatanina_2/MedianFilter.cs
@@ -5,12 +5,31 @@ using System.Text;
 using System.Drawing;
 using System.ComponentModel;
 
+class MyColor : IComparable
+{
+    public int R;
+    public int G;
+    public int B;
+    public MyColor(int R, int G, int B)
+    {
+        this.R = R;
+        this.G = G;
+        this.B = B;
+    }
+
+    public int CompareTo(object obj)
+    {
+        return R + G + B;
+    }
+}
+
 namespace TalamanovBatanina_2
 {
     class MedianFilter : Filters
     {
         private Color medianColor;
         private int size;
+        
 
         public MedianFilter(int size = 3)
         {
@@ -25,13 +44,18 @@ namespace TalamanovBatanina_2
         {
             Bitmap resultImage = new Bitmap(sourseImage.Width, sourseImage.Height);
 
-            var arrayR = new List<int>();
-            var arrayG = new List<int>();
-            var arrayB = new List<int>();
+            //var arrayR = new List<int>();
+            //var arrayG = new List<int>();
+            //var arrayB = new List<int>();
+            var arrayColor = new List<MyColor>();
+            int R;
+            int G;
+            int B;
+            MyColor color;
 
-            
-           
-            
+
+
+
 
             for (int y = size / 2; y < sourseImage.Height - size / 2; y++)
             {
@@ -39,25 +63,34 @@ namespace TalamanovBatanina_2
 
                 for (int x = size / 2; x < sourseImage.Width - size / 2; x++)
                 {
-                    arrayR.Clear();
-                    arrayG.Clear();
-                    arrayB.Clear();
+                    arrayColor.Clear();
+                    //arrayR.Clear();
+                    //arrayG.Clear();
+                    //arrayB.Clear();
 
                     for (int j = -size / 2; j <= size / 2; j++)
                     {
                         for (int i = -size / 2; i <= size / 2; i++)
                         {
-                            arrayR.Add(sourseImage.GetPixel(x + i, y + j).R);
-                            arrayG.Add(sourseImage.GetPixel(x + i, y + j).G);
-                            arrayB.Add(sourseImage.GetPixel(x + i, y + j).B);
+                            R = sourseImage.GetPixel(x + i, y + j).R;
+                            G = sourseImage.GetPixel(x + i, y + j).R;
+                            B = sourseImage.GetPixel(x + i, y + j).R;
+                            color = new MyColor(R, G, B);
+                            arrayColor.Add(color);
+                            //    arrayR.Add(sourseImage.GetPixel(x + i, y + j).R);
+                            //    arrayG.Add(sourseImage.GetPixel(x + i, y + j).G);
+                            //    arrayB.Add(sourseImage.GetPixel(x + i, y + j).B);
+                            //}
                         }
                     }
+                        arrayColor.Sort();
 
-                    arrayR.Sort();
-                    arrayG.Sort();
-                    arrayB.Sort();
 
-                    medianColor = Color.FromArgb(arrayR.ElementAt(size / 2), arrayG.ElementAt(size / 2), arrayB.ElementAt(size / 2));
+                    //arrayR.Sort();
+                    //arrayG.Sort();
+                    //arrayB.Sort();
+
+                    medianColor = Color.FromArgb(arrayColor.ElementAt(size / 2).R , arrayColor.ElementAt(size / 2).G, arrayColor.ElementAt(size / 2).B);
 
                     resultImage.SetPixel(x - size / 2, y - size / 2, calculateNewPixelColor(sourseImage, x - size / 2,y - size / 2));
                 }
